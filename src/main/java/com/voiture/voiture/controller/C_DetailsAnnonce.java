ca4296635ce948b716b4b5e83463b696555aa82b
Laura Ophélie <lauraophelie1@gmail.com>
2024-02-11 06:59:01 +0100

    annonce

--- src/main/java/com/voiture/voiture/controller/C_DetailsAnnonce.java
+++ src/main/java/com/voiture/voiture/controller/C_DetailsAnnonce.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,7 +38,7 @@ public class C_DetailsAnnonce {
         }
     }
 
-    @GetMapping("/save")
+    @PostMapping("/save")
     public ResponseEntity<APIResponse> save(@RequestBody DetailsAnnonce details) {
         try {
             DetailsAnnonce d = service.save(details);
